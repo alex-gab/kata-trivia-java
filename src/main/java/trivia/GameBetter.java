8a4b3bc82a8aa9b14e8557f80615a82d21c6cc0f
Alex Dragoi <dragoi.alex@gmail.com>
2023-04-30 12:39:32 +0300

    eliminate duplicate code

--- src/main/java/trivia/GameBetter.java
+++ src/main/java/trivia/GameBetter.java
@@ -46,16 +46,7 @@ public class GameBetter implements IGame {
             isGettingOutOfPenaltyBox = true;
 
             System.out.println(players.get(currentPlayer) + " is getting out of the penalty box");
-            places[currentPlayer] = places[currentPlayer] + roll;
-            if (places[currentPlayer] > 11) {
-               places[currentPlayer] = places[currentPlayer] - 12;
-            }
-
-            System.out.println(players.get(currentPlayer)
-                               + "'s new location is "
-                               + places[currentPlayer]);
-            System.out.println("The category is " + currentCategory().getName());
-            askQuestion();
+            continueToPlay(roll);
          } else {
             System.out.println(players.get(currentPlayer) + " is not getting out of the penalty box");
             isGettingOutOfPenaltyBox = false;
@@ -63,18 +54,26 @@ public class GameBetter implements IGame {
 
       } else {
 
-         places[currentPlayer] = places[currentPlayer] + roll;
-         if (places[currentPlayer] > 11) {
-            places[currentPlayer] = places[currentPlayer] - 12;
-         }
-
-         System.out.println(players.get(currentPlayer)
-                            + "'s new location is "
-                            + places[currentPlayer]);
-         System.out.println("The category is " + currentCategory().getName());
-         askQuestion();
+         continueToPlay(roll);
       }
 
+   }
+
+   private void continueToPlay(int roll) {
+      calculatePlayerPlace(roll);
+
+      System.out.println(players.get(currentPlayer)
+                         + "'s new location is "
+                         + places[currentPlayer]);
+      System.out.println("The category is " + currentCategory().getName());
+      askQuestion();
+   }
+
+   private void calculatePlayerPlace(int roll) {
+      places[currentPlayer] = places[currentPlayer] + roll;
+      if (places[currentPlayer] > 11) {
+         places[currentPlayer] = places[currentPlayer] - 12;
+      }
    }
 
    private void askQuestion() {
